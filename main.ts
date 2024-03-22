0fe902d01dd8a5b4670d56600ccc22f2c1ad083e
NomanGhaziani <153339067+NomanGhaziani@users.noreply.github.com>
2024-03-22 07:54:32 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+let percentage = 65
+if (percentage>= 90){
+    console.log ('A1 grade')
+} 
+else if(percentage>=80 && percentage<=89.99){
+    console.log ('A grade')
+}
+else if(percentage>=70 && percentage<=79.99){
+    console.log ('B grade')
+}
+else if(percentage>=60 && percentage<=69.99){
+    console.log ('C grade')
+}
+else if(percentage>=50 && percentage <=59.99){
+    console.log ('D grade')
+}
+else{
+    console.log ('Fail')
+}
